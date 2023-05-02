f87bf075ec7986afb8a2c31d6783fb0d3915313f
paulocenrique <paulocenrique@gmail.com>
2023-05-02 19:40:29 -0300

    feat: add SingleChildScrollView

--- why_park/lib/presentation/login/login_screen.dart
+++ why_park/lib/presentation/login/login_screen.dart
@@ -17,115 +17,135 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   bool _passwordIsVisible = false;
 
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final mediaQueryHeight = MediaQuery.of(context).size.height;
     final mediaQueryWidth = MediaQuery.of(context).size.width;
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Padding(
-        padding: const EdgeInsets.all(31.0),
-        child: Column(
-          children: [
-            SizedBox(height: mediaQueryHeight * 0.03),
-            Image.asset('assets/images/logo.png',
-                height: mediaQueryHeight * 0.25, width: mediaQueryWidth * 0.70),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            BlocBuilder<LoginPresenter, LoginState>(
-                bloc: widget._loginPresenter,
-                builder: (final _, final state) {
-                  return Column(
-                    children: [
-                      TextFormField(
-                        onChanged: (value) => widget._loginPresenter
-                            .add(LoginFieldsChangedEvent("email", value)),
-                        decoration: const InputDecoration(
-                          labelText: 'E-mail',
-                          filled: true,
-                        ),
-                      ),
-                      SizedBox(height: mediaQueryHeight * 0.03),
-                      TextFormField(
-                        onChanged: (value) => widget._loginPresenter
-                            .add(LoginFieldsChangedEvent("password", value)),
-                        obscureText: !_passwordIsVisible,
-                        decoration: InputDecoration(
-                          labelText: 'Senha',
-                          filled: true,
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _passwordIsVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _passwordIsVisible = !_passwordIsVisible;
-                              });
-                            },
+        padding: EdgeInsets.fromLTRB(31.0, 0.0, 31.0, bottom),
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Column(
+            children: [
+              SizedBox(height: mediaQueryHeight * 0.03),
+              Image.asset('assets/images/logo.png',
+                  height: mediaQueryHeight * 0.25,
+                  width: mediaQueryWidth * 0.70),
+              SizedBox(height: mediaQueryHeight * 0.03),
+              BlocBuilder<LoginPresenter, LoginState>(
+                  bloc: widget._loginPresenter,
+                  builder: (final _, final state) {
+                    return Column(
+                      children: [
+                        TextFormField(
+                          focusNode: _emailFocusNode,
+                          onChanged: (value) => widget._loginPresenter
+                              .add(LoginFieldsChangedEvent("email", value)),
+                          onFieldSubmitted: (_) => _passwordFocusNode.requestFocus(),
+                          decoration: const InputDecoration(
+                            labelText: 'E-mail',
+                            filled: true,
                           ),
                         ),
-                      ),
-                    ],
-                  );
-                }),
-            SizedBox(height: mediaQueryHeight * 0.01),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () => print('teste'),
-                  child: const Text(
-                    'Esqueci minha senha',
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: mediaQueryHeight * 0.02),
-            OutlinedButton(
-              onPressed: () =>
-                  Navigator.of(context).pushNamed(RoutesTable.home),
-              child: const Text(
-                'Entrar',
+                        SizedBox(height: mediaQueryHeight * 0.03),
+                        TextFormField(
+                          focusNode: _passwordFocusNode,
+                          onChanged: (value) => widget._loginPresenter
+                              .add(LoginFieldsChangedEvent("password", value)),
+                          obscureText: !_passwordIsVisible,
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                            filled: true,
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _passwordIsVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _passwordIsVisible = !_passwordIsVisible;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
+              SizedBox(height: mediaQueryHeight * 0.01),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => print('teste'),
+                    child: const Text(
+                      'Esqueci minha senha',
+                    ),
+                  )
+                ],
               ),
-            ),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            Row(children: [
-              const Expanded(
-                  child: Divider(
-                thickness: 1.0,
-              )),
-              SizedBox(width: mediaQueryWidth * 0.02),
-              const Text(
-                'ou',
-              ),
-              SizedBox(width: mediaQueryWidth * 0.02),
-              const Expanded(
-                  child: Divider(
-                thickness: 1.0,
-              ))
-            ]),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            const OutlinedButton(
-              onPressed: null,
-              child: Text(
-                'Entrar com a conta google',
-              ),
-            ),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Não tem uma conta?'),
-                TextButton(
-                  onPressed: () =>
-                      Navigator.of(context).pushNamed(RoutesTable.signup),
-                  child: const Text('Cadastre-se'),
+              SizedBox(height: mediaQueryHeight * 0.02),
+              OutlinedButton(
+                onPressed: () {
+                  widget._loginPresenter.add(LoginClickedEvent());
+                  Navigator.of(context).pushNamed(RoutesTable.home);
+                },
+                child: const Text(
+                  'Entrar',
                 ),
-              ],
-            )
-          ],
+              ),
+              SizedBox(height: mediaQueryHeight * 0.03),
+              Row(children: [
+                const Expanded(
+                    child: Divider(
+                  thickness: 1.0,
+                )),
+                SizedBox(width: mediaQueryWidth * 0.02),
+                const Text(
+                  'ou',
+                ),
+                SizedBox(width: mediaQueryWidth * 0.02),
+                const Expanded(
+                    child: Divider(
+                  thickness: 1.0,
+                ))
+              ]),
+              SizedBox(height: mediaQueryHeight * 0.03),
+              const OutlinedButton(
+                onPressed: null,
+                child: Text(
+                  'Entrar com a conta google',
+                ),
+              ),
+              SizedBox(height: mediaQueryHeight * 0.03),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Não tem uma conta?'),
+                  TextButton(
+                    onPressed: () =>
+                        Navigator.of(context).pushNamed(RoutesTable.signup),
+                    child: const Text('Cadastre-se'),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- why_park/lib/presentation/signup/signup_screen.dart
+++ why_park/lib/presentation/signup/signup_screen.dart
@@ -16,118 +16,143 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   bool _passwordIsVisible = false;
   bool _confirmPasswordIsVisible = false;
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _nameFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+  final FocusNode _confirmPasswordNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
     final mediaQueryHeight = MediaQuery.of(context).size.height;
     final mediaQueryWidth = MediaQuery.of(context).size.width;
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Padding(
-        padding: const EdgeInsets.all(31.0),
-        child: Column(
-          children: [
-            SizedBox(height: mediaQueryHeight * 0.03),
-            Image.asset('assets/images/logo.png',
-                height: mediaQueryHeight * 0.15, width: mediaQueryWidth * 0.45),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            const Text(
-              'Cadastre-se para encontrar as melhores vagas em estacionamentos.',
-              style: TextStyle(
-                fontSize: 20,
+        padding: EdgeInsets.fromLTRB(31.0, 0.0, 31.0, bottom),
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Column(
+            children: [
+              SizedBox(height: mediaQueryHeight * 0.03),
+              Image.asset('assets/images/logo.png',
+                  height: mediaQueryHeight * 0.15,
+                  width: mediaQueryWidth * 0.45),
+              SizedBox(height: mediaQueryHeight * 0.03),
+              const Text(
+                'Cadastre-se para encontrar as melhores vagas em estacionamentos.',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
-            ),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            BlocBuilder<SignupPresenter, SignupState>(
-                bloc: widget._signupPresenter,
-                builder: (final _, final state) {
-                  return TextFormField(
-                    onChanged: (value) => widget._signupPresenter
-                        .add(SignupFieldsChangedEvent("email", value)),
-                    decoration: InputDecoration(
-                      labelText: 'E-mail',
-                      errorText: state.status == Status.invalid
-                          ? 'Email inválido'
-                          : null,
-                      filled: true,
+              SizedBox(height: mediaQueryHeight * 0.03),
+              BlocBuilder<SignupPresenter, SignupState>(
+                  bloc: widget._signupPresenter,
+                  builder: (final _, final state) {
+                    return Column(
+                      children: [
+                        TextFormField(
+                          focusNode: _emailFocusNode,
+                          onChanged: (value) => widget._signupPresenter
+                              .add(SignupFieldsChangedEvent("email", value)),
+                          onFieldSubmitted: (_) =>
+                              _nameFocusNode.requestFocus(),
+                          decoration: InputDecoration(
+                            labelText: 'E-mail',
+                            errorText: state.status == Status.invalid
+                                ? 'Email inválido'
+                                : null,
+                            filled: true,
+                          ),
+                        ),
+                        SizedBox(height: mediaQueryHeight * 0.03),
+                        TextFormField(
+                          focusNode: _nameFocusNode,
+                          onChanged: (value) => widget._signupPresenter
+                              .add(SignupFieldsChangedEvent("name", value)),
+                          onFieldSubmitted: (_) =>
+                              _passwordFocusNode.requestFocus(),
+                          decoration: const InputDecoration(
+                            labelText: 'Nome',
+                            filled: true,
+                          ),
+                        ),
+                        SizedBox(height: mediaQueryHeight * 0.03),
+                        TextFormField(
+                          focusNode: _passwordFocusNode,
+                          onChanged: (value) => widget._signupPresenter
+                              .add(SignupFieldsChangedEvent("password", value)),
+                          onFieldSubmitted: (_) =>
+                              _confirmPasswordNode.requestFocus(),
+                          obscureText: !_passwordIsVisible,
+                          decoration: InputDecoration(
+                            labelText: 'Digita sua senha',
+                            filled: true,
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _passwordIsVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _passwordIsVisible = !_passwordIsVisible;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: mediaQueryHeight * 0.03),
+                        TextFormField(
+                          focusNode: _confirmPasswordNode,
+                          onChanged: (value) => widget._signupPresenter.add(
+                              SignupFieldsChangedEvent(
+                                  "confirmPassword", value)),
+                          obscureText: !_confirmPasswordIsVisible,
+                          decoration: InputDecoration(
+                            labelText: 'Confirma sua senha',
+                            filled: true,
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _confirmPasswordIsVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _confirmPasswordIsVisible =
+                                      !_confirmPasswordIsVisible;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
+              SizedBox(height: mediaQueryHeight * 0.03),
+              OutlinedButton(
+                onPressed: () =>
+                    widget._signupPresenter.add(SignupClickedEvent()),
+                child: const Text(
+                  'Cadastrar',
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Já tem uma conta?'),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text(
+                      'Acesse',
                     ),
-                  );
-                }),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            TextFormField(
-              onChanged: (value) => widget._signupPresenter
-                  .add(SignupFieldsChangedEvent("name", value)),
-              decoration: const InputDecoration(
-                labelText: 'Nome',
-                filled: true,
-              ),
-            ),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            TextFormField(
-              onChanged: (value) => widget._signupPresenter
-                  .add(SignupFieldsChangedEvent("password", value)),
-              obscureText: !_passwordIsVisible,
-              decoration: InputDecoration(
-                labelText: 'Digita sua senha',
-                filled: true,
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _passwordIsVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _passwordIsVisible = !_passwordIsVisible;
-                    });
-                  },
-                ),
-              ),
-            ),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            TextFormField(
-              onChanged: (value) => widget._signupPresenter
-                  .add(SignupFieldsChangedEvent("confirmPassword", value)),
-              obscureText: !_confirmPasswordIsVisible,
-              decoration: InputDecoration(
-                labelText: 'Confirma sua senha',
-                filled: true,
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _confirmPasswordIsVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _confirmPasswordIsVisible = !_confirmPasswordIsVisible;
-                    });
-                  },
-                ),
-              ),
-            ),
-            SizedBox(height: mediaQueryHeight * 0.03),
-            OutlinedButton(
-              onPressed: () =>
-                  widget._signupPresenter.add(SignupClickedEvent()),
-              child: const Text(
-                'Cadastrar',
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Já tem uma conta?'),
-                TextButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  child: const Text(
-                    'Acesse',
-                  ),
-                )
-              ],
-            )
-          ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
